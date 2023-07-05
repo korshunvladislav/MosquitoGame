007e8ef1519765aa0ac07c7c4833f28a2a54a61d
Vladislav <vladislav.korshun.2005@mail.ru>
2023-07-05 15:22:04 +0300

    GameDev 5.2

--- core/src/com/mygdx/game/MosquitoGame.java
+++ core/src/com/mygdx/game/MosquitoGame.java
@@ -8,10 +8,11 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MosquitoGame extends ApplicationAdapter {
+    public static final int SCR_WIDTH = 1280, SCR_HEIGHT = 720;
     SpriteBatch batch;
     Texture img;
     Texture imgBG;
-    Mosquito[] mosquito = new Mosquito[100000];
+    Mosquito[] mosquito = new Mosquito[100];
 
     @Override
     public void create() {
@@ -20,11 +21,11 @@ public class MosquitoGame extends ApplicationAdapter {
         imgBG = new Texture("bg.jpg");
         for (int i = 0; i < mosquito.length; i++) {
             mosquito[i] = new Mosquito();
-            mosquito[i].x = MathUtils.random(0f, 500);
-            mosquito[i].y = MathUtils.random(0f, 500);
-            mosquito[i].vx = MathUtils.random(2f, 10);
-            mosquito[i].vy = MathUtils.random(2f, 10);
-            mosquito[i].mosquitWH = MathUtils.random(25f , 200);
+            mosquito[i].mosquitWH = MathUtils.random(25f, 200);
+            mosquito[i].x = MathUtils.random(0f, SCR_WIDTH - mosquito[i].mosquitWH);
+            mosquito[i].y = MathUtils.random(0f, SCR_HEIGHT - mosquito[i].mosquitWH);
+            mosquito[i].vx = MathUtils.random(-10f, 10);
+            mosquito[i].vy = MathUtils.random(-10f, 10);
         }
     }
 
@@ -33,14 +34,14 @@ public class MosquitoGame extends ApplicationAdapter {
         for (Mosquito value : mosquito) {
             value.x += value.vx;
             value.y += value.vy;
-            if (value.x > 1280 - value.mosquitWH || value.x < 0)
+            if (value.x >= SCR_WIDTH - value.mosquitWH || value.x <= 0)
                 value.vx = -value.vx;
-            if (value.y > 720 - value.mosquitWH || value.y < 0)
+            if (value.y >= SCR_HEIGHT - value.mosquitWH || value.y <= 0)
                 value.vy = -value.vy;
         }
         ScreenUtils.clear(0, 1, 0, 1);
         batch.begin();
-        batch.draw(imgBG, 0, 0, 1280, 720);
+        batch.draw(imgBG, 0, 0, SCR_WIDTH, SCR_HEIGHT);
         for (Mosquito value : mosquito) {
             batch.draw(img, value.x, value.y, value.mosquitWH, value.mosquitWH);
         }
